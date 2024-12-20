25670aa1af4d1908b8bb9c0670928b217ec4c861
sagarkudutata <sagar.kudu@tatasustainability.com>
2024-12-20 18:43:33 +0530

    task-2

--- project/packages/ui/components/List.tsx
+++ project/packages/ui/components/List.tsx
@@ -1,16 +1,12 @@
 
-export const List = ({ data }) => {
-  return (
-    <div>
-      <p>List</p>
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>
-            {JSON.stringify(item)}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+export const List = ({data}) => {
+  return <div>
+  <p>List</p>
+  <ul>
+    {data.map((item, index) => (
+      <li key={index}>{item.name}</li>
+    ))}
+    </ul>
+  </div>
+}
 
